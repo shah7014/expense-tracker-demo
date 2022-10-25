bf713135ef8c27b4ff581d9bfc8bd1d066b7b105
Romil Shah <romilshah2199@gmail.com>
2022-10-25 18:12:24 +0530

    feature: loading static expenses data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/expenses/Expenses";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const expenses = [
+    {
+      id: "e1",
+      title: "Insurance",
+      amount: 300,
+      date: new Date(2021, 2, 20),
+    },
+    {
+      id: "e2",
+      title: "Study",
+      amount: 200,
+      date: new Date(2021, 4, 24),
+    },
+    {
+      id: "e3",
+      title: "Books",
+      amount: 250,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: "e4",
+      title: "Bag",
+      amount: 400,
+      date: new Date(2021, 7, 19),
+    },
+  ];
+
+  return <Expenses expenses={expenses} />;
 }
 
 export default App;
